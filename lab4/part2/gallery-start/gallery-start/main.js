0377aeabc3a831566f219af934fe9f8762ac1772
Mohamed <Mohamed.haniff@dcmail.ca>
2025-04-07 23:36:21 -0400

    added loop through the image and also click event to change the text also

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -6,13 +6,27 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-/* Declaring the alternative text for each image file */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'] ; 
 
+/* Declaring the alternative text for each image file */
+const altText = {  'pic1.jpg': 'Closeup of a blue human eye',
+    'pic2.jpg': 'Rock that looks like a wave',
+    'pic3.jpg': 'Purple and white pansies',
+    'pic4.jpg': 'Section of wall from a pharaoh\'s tomb',
+    'pic5.jpg': 'Large moth on a leaf'
+
+}; 
 /* Looping through images */
 
+for (const fileName of imageFilenames) { 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', 'images/${fileName} ' );
+newImage.setAttribute('alt', altText [fileName]);
 thumbBar.appendChild(newImage);
+newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', `images/${fileName}`);
+    displayedImage.setAttribute('alt', altText[fileName]);
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
