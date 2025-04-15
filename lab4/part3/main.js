bf635b7158a5703375809781ddeddf94f128ea1a
Mohamed <Mohamed.haniff@dcmail.ca>
2025-04-14 22:57:19 -0400

    drew the ball

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -26,4 +26,15 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
